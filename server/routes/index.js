e86728666fad317bed7c387b3cff5397ddaf6b24
William <xf0718@gmail.com>
2016-03-13 00:14:46 +0800

    新建一个简单的登陆界面

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/login', function(req, res, next) {
+    res.render('login');
+});
+
+router.post('/login.do', function(req, res, next) {
+    res.send(req.body);
+    console.log('username = %s', req.body.username);
+    console.log('password = %s', req.body.password);
+});
+
 module.exports = router;
