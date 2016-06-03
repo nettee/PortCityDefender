f12d8909455d2f75b9cfe069e2e2269a297c1e29
penguinfirst <zhangcx_good@126.com>
2016-06-03 11:35:27 +0800

    A little Update

--- client/www/js/controllers.js
+++ client/www/js/controllers.js
@@ -32,34 +32,43 @@ angular.module('ionicApp.controllers', ['ionicApp.services'])
 })
 
 
-.controller('ContactsController', function ($scope, $http, userService) {
-  var regionlist = ['south', 'west'];
-  /*
+.controller('ContactsController', function ($scope, $http, userService, regionService) {
+  var regionlist = [];
+  $scope.groups = [];
+
   $http.get("http://121.40.97.40:3000/regions")
     .success(function (response) {
       regionlist = response;
+      for (var i = 0; i < regionlist.length; i++) {
+        $scope.groups[i] = {
+          name: regionlist[i],
+          items: [],
+          show: false
+        };
+      }
     })
     .error(function (response) {
       alert("Fail to get the regions");
       console.log("app ConstactsController Fail to get regions --- error message : ", response.error);
-    })*/
+    })
 
-  $scope.groups = [];
 
+/*
   for (var i = 0; i < regionlist.length; i++) {
+    alert(1);
     $scope.groups[i] = {
       name: regionlist[i],
       items: [],
       show: false
     };
-
+    console.log($scope.groups[i].name);
     var userlist = userService.getUsersByRegion(regionlist[i]);
 
     for (var j = 0; j < userlist.length; j++) {
       var usertemp = [userlist[j].id,userlist[j].name,userlist[j].level];
       $scope.groups[i].items.push(usertemp);
     }
-  }
+  }*/
 
   /*
    * if given group is the selected group, deselect it
@@ -67,10 +76,36 @@ angular.module('ionicApp.controllers', ['ionicApp.services'])
    */
   $scope.toggleGroup = function(group) {
     group.show = !group.show;
-  };
+    for (var i = 0; i < regionlist.length; i++) {
+      $http.get("http://121.40.97.40:3000/users?region=" + regionlist[i])
+        .success(function (response) {
+          for (var j = 0;j < response.length;i++){
+            $scope.groups[0].items.push(response[j]);
+          }
+        })
+        .error(function (response) {
+          alert("Fail to get the users by region : " + region);
+          console.log("app userService method-getUserByRegion Fail to get---error message : ", response.error);
+        })
+    }
+  }
   $scope.isGroupShown = function(group) {
     return group.show;
   };
+
+  /**
+   * Below is the search
+   */
+  var searchisnull = true;
+  var searchResults = [];
+  $scope.search = function (searchcontent) {
+    if (searchcontent == '')
+      searchisnull = true;
+    else {
+      searchResults = userService.filterUserByName(searchcontent);
+      searchisnull = false;
+    }
+  }
 })
 
 .controller('ContactController', function ($scope,$stateParams, userService) {
--- client/www/js/services.js
+++ client/www/js/services.js
@@ -53,9 +53,10 @@ app.factory('userService', function($http){
 
   var usersInRegion = function (region) {
     var userlist = [];
-
+    //alert(region);
     $http.get("http://121.40.97.40:3000/users?region=" + region)
       .success(function (response) {
+        //console.log(response);
         for (var i = 0;i < response.length;i++){
           var usertemp = new newuser(response[i].id,response[i].name,response[i].level,response[i].region,response[i].description,response[i].phone);
           userlist.push(usertemp);
@@ -97,13 +98,13 @@ app.factory('userService', function($http){
 
   return {
     getUsersByRegion : function (region) {
-      //return usersInRegion(region);
-      var userlist = [];
+      return usersInRegion(region);
+      /*var userlist = [];
       var temp = new regionuser("txp","陶先平",2);
       userlist.push(temp);
       temp = new regionuser("yinyt","尹一通",3);
       userlist.push(temp);
-      return userlist;
+      return userlist;*/
     },
 
     getUser : function () {
@@ -117,6 +118,45 @@ app.factory('userService', function($http){
     setUsername : function(id){
       user.id = id;
       fillUser(id);
+    },
+
+    filterUserByName: function (searchcontent) {
+      var userlist = [];
+
+      $http.get("http://121.40.97.40:3000/users")
+        .success(function (response) {
+          console.log("app service filterUserByName response : " + response);
+          for (var i in response){
+            if (response[i].name.indexOf(searchcontent) != -1){
+              userlist.push(response[i]);
+            }
+          }
+        })
+        .error(function (response) {
+          alert("Fail to get the users" + region);
+          console.log("app userService method-filter Fail to get---error message : ", response.error);
+        })
+
+      return userlist;
     }
   };
 });
+
+app.factory('regionService', function ($http) {
+  return {
+    getRegion:function () {
+      var regionlist = [];
+      $http.get("http://121.40.97.40:3000/regions")
+        .success(function (response) {
+          console.log("1 : " + response);
+          regionlist = response;
+        })
+        .error(function (response) {
+          alert("Fail to get the regions");
+          console.log("app ConstactsController Fail to get regions --- error message : ", response.error);
+        })
+      console.log("1 : " + regionlist);
+      return regionlist;
+    }
+  }
+})
