f0d12b5dcf70655832e48544494aafb016d7e5b8
NoraLigithub <NJUlinan@163.com>
2016-07-08 11:34:55 +0630

    修复发送命令时的bug

--- client/www/js/services.js
+++ client/www/js/services.js
@@ -1,5 +1,6 @@
 var app = angular.module('ionicApp.services',[])
-var ipAddress = "http://localhost:3000"//http://121.40.97.40:3000
+//var ipAddress = "http://localhost:3000"
+var ipAddress = "http://121.40.97.40:3000"
 
 app.factory('userService', function($http){
   /**A user contains 6 properties
@@ -193,14 +194,16 @@ app.factory('commandService',function($http,userService){
           content:content
         }
 
-        console.log("in send command"+command.receiver+"  "+command.sender+"  "+command.content);
-        $http.post(ipAddress + "/commands",command)
+        console.log("in send command"+command[i].receiver+"  "+command[i].sender+"  "+command[i].content);
+        $http.post(ipAddress + "/commands",command[i])
 
          .success(function(response){
            console.log(response.updated_time);
          })
           .error(function(response){
+            alert("send failed")
           })
+          
       }
       receiverList=[];
       setAllContactsNotChoose();
